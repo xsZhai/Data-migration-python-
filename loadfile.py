09f94df565de074b6965dd7e557b3f59caa13024
XIAOSONG ZHAI <Xiaosong.zhai@gmail.com>
2020-10-26 12:03:27 -0400

    Update loadfile.py

--- loadfile.py
+++ loadfile.py
@@ -1,53 +1,12 @@
 import pandas as pd
 from unidecode import unidecode
-
 import os
-# filenames = os.listdir('originFile')
-# filenames = [f for f in filenames if f.endswith('.xlsx')]
-
-# for filename in filenames:
-#     print(filename)
-#     df = pd.read_excel('originFile/'+filename, skiprows=1)
-#     df.to_csv('csvfiles/'+filename.replace('.xlsx', '.csv'), index=0)
-#     with open('csvfiles/'+filename.replace('.xlsx', '.csv'),'r+') as f:
-#         text = f.read()
-#         text0 = unidecode(text)
-#         f.truncate(0)
-#         f.write(text0)
-
-
-# import findspark
-# findspark.init()
-# from pyspark.sql import SparkSession
-# spark = SparkSession.builder.getOrCreate()
-
-
-# df = spark.read.csv('/var/rel8ed.to/buldwith/csvfiles/*', header=True, escape='"')
-
-
-# pdf = pd.read_csv('/var/rel8ed.to/buldwith/csvfiles/All-Live-Adobe-CQ-Sites.csv', dtype='str')
-# pdf = pdf.rename(columns={pdf.columns[0]:'Domain'})
-
-# from sqlalchemy import create_engine
-
-# e = create_engine('mysql://root:rel8edto@localhost/buildwith')
-
-# pdf.to_sql('buildwith0', e)
-
-# conn = e.connect()
-# rs = conn.execute('select * from buildwith0 limit 10')
-
-# dfall = pd.read_csv('/var/rel8ed.to/buldwith/buildwith.csv', dtype='str')
-# e = create_engine('mysql://root:rel8edto@localhost/buildwith?charset=latin1')
-
-# dfall.to_sql('buildwithI', e)
-
-# #
-# pdf.columns
 from sqlalchemy import create_engine
+
+
 filenames = os.listdir('csvfiles')
 filenames = [f for f in filenames if f.endswith('.csv')]
-e = create_engine('mysql://root:rel8edto@localhost/buildwith')
+e = create_engine('*****')
 fail = open('failedsites.csv', 'a')
 for filename in filenames:
     print(filename)
